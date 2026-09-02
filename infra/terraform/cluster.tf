172d31338bebf86385b563b9067235a10c6338b7
dileep <dileepkushwaha@hotmail.com>
2026-09-02 17:05:42 +0530

    feat(terraform): add GKE dev cluster with spot node pool

---
+++ infra/terraform/cluster.tf
@@ -0,0 +1,40 @@
+resource "google_container_cluster" "dev" {
+  name     = "gke-poc-dev"
+  location = "var.zone"
+  network = module.vpc.network_id
+  subnetwork = module.vpc.subnet_id
+  networking_mode = "VPC_NATIVE"
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = "10.1.0.0/16"
+    services_ipv4_cidr_block = "10.2.0.0/16"
+  }
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = var.authorized_networks_cidr
+      display_name = "my-ip"
+    }
+  }
+
+  deletion_protection = false
+}
